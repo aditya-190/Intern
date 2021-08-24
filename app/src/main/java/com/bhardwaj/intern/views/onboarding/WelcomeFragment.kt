e83d146b7bdbbbbaeac5cf47ea24146bb79a1e09
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-08-25 03:27:37 +0530

    Navigation Graph Set. OnBoarding Images Added. Sign In and Sign Up Button Operational.

--- app/src/main/java/com/bhardwaj/intern/views/onboarding/WelcomeFragment.kt
+++ app/src/main/java/com/bhardwaj/intern/views/onboarding/WelcomeFragment.kt
@@ -4,14 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.bhardwaj.intern.R
 
 class WelcomeFragment : Fragment() {
+    private lateinit var navController: NavController
+    private lateinit var tvSignUp: TextView
+    private lateinit var tvSignIn: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvSignUp = view.findViewById(R.id.tvSignUp)
+        tvSignIn = view.findViewById(R.id.tvSignIn)
+        navController = view.findNavController()
+
+        tvSignIn.setOnClickListener {
+            navController.navigate(WelcomeFragmentDirections.actionWelcomeFragmentToLoginFragment())
+        }
+        tvSignUp.setOnClickListener {
+            navController.navigate(WelcomeFragmentDirections.actionWelcomeFragmentToSignUpFragment())
+        }
     }
 }
